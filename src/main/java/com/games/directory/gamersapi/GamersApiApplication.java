12218c048c9c55fbb8fc0f735258c335c5d44312
Lucas Saldanha <asterix@Lucass-Mac-mini.local>
2022-06-28 22:54:07 +0200

    initial commit

---
+++ src/main/java/com/games/directory/gamersapi/GamersApiApplication.java
@@ -0,0 +1,13 @@
+package com.games.directory.gamersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamersApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/games/directory/gamersapi/GamersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.directory.gamersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
