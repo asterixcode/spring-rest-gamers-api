ecc7f092cb8f2612c5e9bb490bb8cc2e702d7d1a
lucas-saldanha <lk.saldanha@gmail.com>
2022-07-01 01:02:31 +0200

    added exception handling

---
+++ src/main/java/com/gamersdirectory/gamersapi/exception/ApiException.java
@@ -0,0 +1,31 @@
+package com.gamersdirectory.gamersapi.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+public class ApiException extends Exception {
+
+    private final String message;
+    private final HttpStatus httpStatus;
+    private final ZonedDateTime timestamp;
+
+    public ApiException(String message, HttpStatus httpStatus, ZonedDateTime timestamp) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public ZonedDateTime getTimestamp() {
+        return timestamp;
+    }
+}
